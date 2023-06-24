5d0adf4636ae658548f3e6628771bf1d56fbd665
ericaraujo13 <ericpoloni@gmail.com>
2023-06-24 00:55:27 -0500

    finish

---
+++ db/migrate/20230624043222_create_todos.rb
@@ -0,0 +1,10 @@
+class CreateTodos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :todos do |t|
+      t.string :title, null: false
+      t.integer :status, default: 0
+
+      t.timestamps
+    end
+  end
+end
